10e929b5d053935ae1c39162b03fb8c5ac4526fb
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-17 22:50:12 +0800

    优化插件的订阅源，用immortalwrt库的文件替换openwrt库的文件

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 
-# 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-homeproxy
-
-#echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
+svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
 
-echo 'src-git kenzo8 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
-git clone --depth=1 https://github.com/Joecaicai/luci-theme-ifit package/luci-theme-ifit 
+rm -rf feeds/luci/modules/luci-base
+rm -rf feeds/luci/modules/luci-mod-status
+rm -rf feeds/packages/utils/coremark
+rm -rf package/emortal/default-settings
+
+svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-base feeds/luci/modules/luci-base
+svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-mod-status feeds/luci/modules/luci-mod-status
+svn export https://github.com/immortalwrt/packages/branches/master/utils/coremark package/new/coremark
+svn export https://github.com/immortalwrt/immortalwrt/branches/master/package/emortal/default-settings package/emortal/default-settings
+# svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/utils/mhz package/utils/mhz
 echo "COMMIT_HASH=$(git rev-parse HEAD)" >> $GITHUB_ENV
         
 #mv $GITHUB_WORKSPACE/path/0001-ipq807x-add-stock-layout-variant-for-redmi-ax6.patch 0001-ipq807x-add-stock-layout-variant-for-redmi-ax6.patch
 #git apply 0001-ipq807x-add-stock-layout-variant-for-redmi-ax6.patch
         
-chmod +x $GITHUB_WORKSPACE/path/*.sh
-chmod -Rf 777 $GITHUB_WORKSPACE/path/*
-chmod -Rf 777 $GITHUB_WORKSPACE/path/*.*
+chmod +x $GITHUB_WORKSPACE/patch/*.sh
+chmod -Rf 755 $GITHUB_WORKSPACE/patch/*
 mv $GITHUB_WORKSPACE/path/01_leds target/linux/qualcommax/ipq807x/base-files/etc/board.d/01_leds
 mv $GITHUB_WORKSPACE/path/02_network target/linux/qualcommax/ipq807x/base-files/etc/board.d/02_network
 mv $GITHUB_WORKSPACE/path/11-ath10k-caldata target/linux/qualcommax/ipq807x/base-files/etc/hotplug.d/firmware/11-ath10k-caldata
