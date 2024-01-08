db08e64b76844420958236f2e3ef62c92766b4cf
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-08 15:49:34 +0800

    生成armv8专有rootfs.tar.gz底包的模块

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -12,15 +12,15 @@ rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-homeproxy
 
-echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
-svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
+#echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
+#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
+#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
 
 #############################################################################################
 
-#echo 'src-git kenzo8 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
-#git clone --depth=1 https://github.com/Joecaicai/luci-theme-ifit package/luci-theme-ifit 
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
+git clone --depth=1 https://github.com/Joecaicai/luci-theme-ifit package/luci-theme-ifit 
 echo "COMMIT_HASH=$(git rev-parse HEAD)" >> $GITHUB_ENV
 
