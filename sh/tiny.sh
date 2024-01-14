9e29bdf42ba2a4d5aaaae5ddc4a36b3f19fab755
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-14 08:16:54 +0800

    immortalwrt N1订阅件

---
+++ sh/tiny.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# 移除要替换的包
+rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-homeproxy
+
+# 科学上网插件
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
+#echo 'src-git mo_app https://github.com/rmoyulong/mo_app' >> feeds.conf.default
+
+# Themes
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 https://github.com/Joecaicai/luci-theme-ifit package/luci-theme-ifit 
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+
+# 取消主题默认设置
+find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+
+#Open Clash
+cd ./package
+
+git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
+#预置OpenClash内核和GEO数据
+export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
+export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
+export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
+export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+
+export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
+export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+
+export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
+export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+
+cd ./OpenClash/luci-app-openclash/root/etc/openclash
+
+curl -sfL -o ./Country.mmdb $GEO_MMDB
+curl -sfL -o ./GeoSite.dat $GEO_SITE
+curl -sfL -o ./GeoIP.dat $GEO_IP
+
+mkdir ./core && cd ./core
+
+curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
+gzip -d ./tun.gz && mv ./tun ./clash_tun
+
+curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
+tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
+
+curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
+tar -zxf ./dev.tar.gz
+
+chmod +x ./clash* ; rm -rf ./*.gz
