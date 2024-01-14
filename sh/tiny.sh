20cd5d64a1712b4ce898c94d66b5c883a224b8a2
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-14 23:45:23 +0800

    snv失效，合并插件订阅源

--- sh/tiny.sh
+++ sh/tiny.sh
@@ -16,7 +16,7 @@ rm -rf feeds/luci/applications/luci-app-homeproxy
 
 # 科学上网插件
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 echo 'src-git mo_app https://github.com/rmoyulong/mo_app' >> feeds.conf.default
 
 # Themes
