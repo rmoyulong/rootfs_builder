9eaeddcfe0d70ef41c8e89be0e565e890a600d3e
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-21 23:55:57 +0800

    snv模拟订阅源

--- sh/quick.sh
+++ sh/quick.sh
@@ -24,90 +24,98 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# 添加额外插件
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+merge_package(){
+    # 参数1是分支名,参数2是库地址,参数3是子路径。所有文件下载到openwrt/package/openwrt-packages路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch=$1 curl=$2 && shift 2
+    rootdir=$(pwd)
+    localdir=package/openwrt-packages
+    [ -d $localdir ] || mkdir -p $localdir
+    tmpdir=$(mktemp -d) || exit 1
+    git clone -b $branch --depth 1 --filter=blob:none --sparse $curl $tmpdir
+    cd $tmpdir
+    git sparse-checkout init --cone
+    git sparse-checkout set $@
+    mv -f $@ $rootdir/$localdir && cd $rootdir
+}
 
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-filebrowser
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-ssr-mudb-server
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-eqos
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-ramfree
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-kodexplorer
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-xlnetacc
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-pushbot
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-qbittorrent
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-wrtbwmon
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-unblockmusic
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-unblockneteasemusic
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-vlmcsd
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-zerotier
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-xunlei
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-adbyby-plus
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-n2n
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-homebox
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-vsftpd
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-easymesh
+# 添加额外插件
+git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/openwrt-packages/luci-app-serverchan
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/openwrt-packages/luci-app-adguardhome
+git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/openwrt-packages/luci-app-ikoolproxy
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/openwrt-packages/luci-app-poweroff
+git clone --depth=1 https://github.com/destan19/OpenAppFilter package/openwrt-packages/OpenAppFilter
+git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/openwrt-packages/luci-app-netdata
+merge_package master https://github.com/lisaac/luci-app-dockerman applications/luci-app-dockerman
+
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/luci-app-filebrowser
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/luci-app-ssr-mudb-server
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/luci-app-eqos
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/luci-app-ramfree
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/luci-app-kodexplorer
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/luci-app-xlnetacc
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/luci-app-pushbot
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/luci-app-qbittorrent
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-wrtbwmon
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-unblockmusic
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-unblockneteasemusic
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-vlmcsd
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-zerotier
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-xunlei
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-adbyby-plus
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-n2n
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-homebox
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-turboacc
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-usb-printer
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-vsftpd
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/luci-app-easymesh
 
 # 科学上网插件
-git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
+git clone --depth=1 https://github.com/immortalwrt/homeproxy package/openwrt-packages/luci-app-homeproxy
+git clone --depth=1 -b master https://github.com/kenzok8/small package/openwrt-packages/small
+#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+merge_package master https://github.com/vernesong/OpenClash applications/luci-app-openclash
+
 
 # 科学上网插件依赖
-git_sparse_clone main https://github.com/kenzok8/small-package v2dat
-git_sparse_clone main https://github.com/kenzok8/small-package lua-maxminddb
-git_sparse_clone main https://github.com/kenzok8/small-package brook
-git_sparse_clone main https://github.com/kenzok8/small-package trojan-go
-git_sparse_clone main https://github.com/kenzok8/small-package trojan-plus
-git_sparse_clone main https://github.com/kenzok8/small-package sagernet-core
-git_sparse_clone main https://github.com/kenzok8/small-package lua-neturl
-git_sparse_clone main https://github.com/kenzok8/small-package redsocks2
-git_sparse_clone main https://github.com/kenzok8/small-package filebrowser
-git_sparse_clone main https://github.com/kenzok8/small-package qBittorrent-Enhanced-Edition
-git_sparse_clone main https://github.com/kenzok8/small-package qBittorrent-static
-git_sparse_clone main https://github.com/kenzok8/small-package qBittorrent
-git_sparse_clone main https://github.com/kenzok8/small-package qt6base
-git_sparse_clone main https://github.com/kenzok8/small-package qtbase
-git_sparse_clone main https://github.com/kenzok8/small-package qttools
-git_sparse_clone main https://github.com/kenzok8/small-package rblibtorrent
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages vlmcsd
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages sing-box
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages UnblockNeteaseMusic-Go
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages UnblockNeteaseMusic
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages wrtbwmon
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages homebox
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages vsftpd-alt
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages ddns-scripts
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages n2n
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages adbyby
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages dns-forwarder
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages upx
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages aria2
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages ariang
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/lua-maxminddb
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/sagernet-core
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/filebrowser
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/qBittorrent-Enhanced-Edition
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/qBittorrent-static
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/qBittorrent
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/qt6base
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/qtbase
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/qttools
+merge_package main https://github.com/kenzok8/small-package package/openwrt-packages/rblibtorrent
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/vlmcsd
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/UnblockNeteaseMusic-Go
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/UnblockNeteaseMusic
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/wrtbwmon
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/homebox
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/vsftpd-alt
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/ddns-scripts
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/n2n
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/adbyby
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/dns-forwarder
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/upx
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/aria2
+merge_package master https://github.com/kiddin9/openwrt-packages package/openwrt-packages/ariang
 
 # Themes
-git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git_sparse_clone main https://github.com/kenzok8/small-package luci-theme-ifit 
+merge_package master https://github.com/kenzok8/luci-theme-ifit luci-theme-ifit 
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 
 # 晶晨宝盒
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-#sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/ophub/luci-app-amlogic'|g" package/luci-app-amlogic/root/etc/config/amlogic
-#sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
-#sed -i "s|ARMv8|ARMv8_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
+sed -i "/amlogic_firmware_repo/ { s|https://github.com/breakings/OpenWrt|https://github.com/rmoyulong/rootfs_builder|g }" package/openwrt-packages/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|ARMv8|4jarmv8|g" package/openwrt-packages/luci-app-amlogic/root/etc/config/amlogic
 
 # SmartDNS
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
@@ -127,8 +135,6 @@ git_sparse_clone main https://github.com/kenzok8/small-package luci-app-ddnsto
 git_sparse_clone main https://github.com/kenzok8/small-package ddnsto
 
 # Alist
-#git_sparse_clone main https://github.com/kenzok8/small-package luci-app-alist
-#git_sparse_clone main https://github.com/kenzok8/small-package alist
 #git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 # iStore
@@ -143,13 +149,7 @@ chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 
 # 取消主题默认设置
 find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
-
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
-# 临时修复acpid,xfsprogs,aliyundrive-webdav
-sed -i 's#flto#flto -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/acpid/Makefile
-sed -i 's#SYNC#SYNC -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/xfsprogs/Makefile
-#sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
-			
+		
 #Open Clash
 cd ./package
 
