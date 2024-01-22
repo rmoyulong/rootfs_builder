d90ddb2e8a15fdec0b112c77396b8a9a9fb956ee
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-22 16:00:48 +0800

    Update tiny.sh

--- sh/tiny.sh
+++ sh/tiny.sh
@@ -28,6 +28,7 @@ function git_sparse_clone() {
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 echo 'src-git mo_app https://github.com/rmoyulong/mo_app' >> feeds.conf.default
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-qbittorrent
 
 #Open Clash
 cd ./package
