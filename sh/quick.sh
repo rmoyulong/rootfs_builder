75210269f8e57f1d9614bf19c02ab976b706f9f3
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-22 17:33:53 +0800

    snv模拟订阅源

--- sh/quick.sh
+++ sh/quick.sh
@@ -94,7 +94,7 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages n2n
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages adbyby
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages dns-forwarder
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages upx
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages ucl
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages ucl
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
