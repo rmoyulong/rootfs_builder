5f551ea66414f72e289d50e5162b2ce35cc98ad5
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-22 10:39:19 +0800

    生成armv8专有rootfs.tar.gz底包的模块

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -12,14 +12,6 @@ rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-homeproxy
 
-#echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
-
-#############################################################################################
-
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
@@ -32,4 +24,42 @@ function git_sparse_clone() {
 
 echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
 git_sparse_clone main https://github.com/kenzok8/small-package luci-theme-ifit 
+
+
+#Open Clash
+cd ./package
+
+git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
+#预置OpenClash内核和GEO数据
+export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
+export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
+export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
+export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+
+export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
+export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+
+export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
+export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+
+cd ./OpenClash/luci-app-openclash/root/etc/openclash
+
+curl -sfL -o ./Country.mmdb $GEO_MMDB
+curl -sfL -o ./GeoSite.dat $GEO_SITE
+curl -sfL -o ./GeoIP.dat $GEO_IP
+
+mkdir ./core && cd ./core
+
+curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
+gzip -d ./tun.gz && mv ./tun ./clash_tun
+
+curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
+tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
+
+curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
+tar -zxf ./dev.tar.gz
+
+chmod +x ./clash* ; rm -rf ./*.gz
+
 
