45fe0bc4aa5da172dfd9111a4c89bd86c5e95ce5
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-22 15:56:34 +0800

    Update tiny.sh

--- sh/tiny.sh
+++ sh/tiny.sh
@@ -14,6 +14,16 @@ rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-homeproxy
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
 # 科学上网插件
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >> feeds.conf.default
