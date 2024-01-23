e4f468607047008ab7b3021f7abfa4d50feb80ad
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-23 15:19:42 +0800

    生成armv8专有rootfs.tar.gz底包的模块

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-# 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-homeproxy
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
-git_sparse_clone main https://github.com/kenzok8/small-package luci-theme-ifit 
-
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >> feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
+echo 'src-git routing https://github.com/coolsnowwolf/routing' >> feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
 
 #Open Clash
 cd ./package
