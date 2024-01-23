b87610618f800fc6bf48b8f0f70c6bb61e27c618
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-23 15:33:38 +0800

    生成armv8专有rootfs.tar.gz底包的模块

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm ./feeds.conf.default
 echo 'src-git packages https://github.com/coolsnowwolf/packages' >> feeds.conf.default
 echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
 echo 'src-git routing https://github.com/coolsnowwolf/routing' >> feeds.conf.default
